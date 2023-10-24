eec1d7b5cd54c0f435654a79e13495e0fd4d895d
Camila <cpgiudice@hotmail.com>
2023-10-24 00:39:07 -0300

    Cambios Login

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/LoginActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/LoginActivity.java
@@ -7,18 +7,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity{
 
+    private EditText editTxtMail;
+    private EditText editTextPass;
+    private Button btnIniciarSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Linkeo
+        editTxtMail=findViewById(R.id.editTextTextEmailAddress);
+        editTextPass=findViewById(R.id.editTextTextPassword);
+        btnIniciarSesion=findViewById(R.id.btnlogin);
+
+        //Para loguearse : llamo al método de mi clase lógica
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTxtMail.getText().toString();
+                String password = editTextPass.getText().toString();
+
+
+                MainActivity mainActivity = new MainActivity();
+
+                if (mainActivity.login(email,password)){
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    //inicio de sesion exitoso
+                    Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    //Inicio de sesion falló
+                    Toast.makeText(LoginActivity.this,"Error en el inicio de sesión", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
 
     }
 
