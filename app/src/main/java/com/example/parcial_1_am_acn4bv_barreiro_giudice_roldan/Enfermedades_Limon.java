213742f77a695bfad7ea13969c058e41ae556b25
monabarreiro <barreiro.monica@gmail.com>
2023-11-28 00:33:14 -0300

    Creacion de activitys enfermedades Limon,Trigo,Uva y Maiz

---
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
@@ -0,0 +1,14 @@
+package com.example.parcial_1_am_acn4bv_barreiro_giudice_roldan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Enfermedades_Limon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_enfermedades_limon);
+    }
+}
---
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Trigo.java
@@ -0,0 +1,6 @@
+package com.example.parcial_1_am_acn4bv_barreiro_giudice_roldan;
+
+import android.app.Activity;
+
+public class Enfermedades_Trigo extends Activity {
+}
