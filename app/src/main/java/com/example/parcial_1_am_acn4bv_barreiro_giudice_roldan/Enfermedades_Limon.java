d6ec4424455e9791e9d800a40ab04307f3146a93
monabarreiro <barreiro.monica@gmail.com>
2023-11-29 11:17:47 -0300

    Definicion de botones en clases enfermedades

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
@@ -3,12 +3,23 @@ package com.example.parcial_1_am_acn4bv_barreiro_giudice_roldan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+// HERNAN
 public class Enfermedades_Limon extends AppCompatActivity {
+    public Button btnAmain;
+    public Button btnAtrigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_enfermedades_limon);
+
+        btnAmain=findViewById (R.id.btnAmain);
+        btnAtrigo=findViewById (R.id.btnAtrigo);
+
+
+
     }
 }
--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
@@ -2,20 +2,27 @@ package com.example.parcial_1_am_acn4bv_barreiro_giudice_roldan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+
 
 
 // HERNAN
 public class Enfermedades_Maiz extends AppCompatActivity {
-    public ImageButton btnAmain;
-    public ImageButton btnAuva;
+    public Button btnAmain;
+    public Button btnAuva;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_enfermedades_maiz);
 
+        btnAmain= findViewById (R.id.btnAmain);
+        btnAuva= findViewById (R.id.btnAuva);
 
 
 
