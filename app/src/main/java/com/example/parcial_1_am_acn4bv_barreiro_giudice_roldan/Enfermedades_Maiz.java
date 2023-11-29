63541cf0dd9ce79a695f42d1df02d61095278ea5
monabarreiro <barreiro.monica@gmail.com>
2023-11-29 10:56:06 -0300

    Definicion de botones en clases enfermedades

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
@@ -3,12 +3,22 @@ package com.example.parcial_1_am_acn4bv_barreiro_giudice_roldan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
+
+// HERNAN
 public class Enfermedades_Maiz extends AppCompatActivity {
+    public ImageButton btnAmain;
+    public ImageButton btnAuva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_enfermedades_maiz);
+
+
+
+
+
     }
 }
