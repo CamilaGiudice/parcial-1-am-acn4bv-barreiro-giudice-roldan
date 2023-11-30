fdc171359445a1daa872cf9ed96f9d0c95aa75af
Camila <cpgiudice@hotmail.com>
2023-11-30 18:20:33 -0300

    Cambios Clase Enfermedad maiz: URL del nuevo método

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
@@ -45,7 +45,7 @@ public class Enfermedades_Maiz extends AppCompatActivity {
         panojaloca.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(""));
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://herbariofitopatologia.agro.uba.ar/?page_id=5267"));
                 startActivity(intent);
             }
         });
