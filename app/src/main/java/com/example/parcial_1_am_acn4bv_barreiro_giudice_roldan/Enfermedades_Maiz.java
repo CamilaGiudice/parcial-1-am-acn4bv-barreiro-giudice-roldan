cfc9c4122bf416702b5bc7dfc50318dd99e91e03
Camila <cpgiudice@hotmail.com>
2023-11-30 18:17:40 -0300

    Cambios Clase Enfermedad maiz: Linkeo el nuevo boton de Panoja Loca

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Maiz.java
@@ -16,7 +16,7 @@ public class Enfermedades_Maiz extends AppCompatActivity {
     public Button btnAuva;
 
     // Declaro las variables con las que voy a trabajar
-    private Button cuarto,carbon,blanca,nitrogeno,roya;
+    private Button cuarto,carbon,blanca,nitrogeno,roya, panojaloca;
 
 
     @SuppressLint("WrongViewCast")
@@ -31,6 +31,7 @@ public class Enfermedades_Maiz extends AppCompatActivity {
         blanca= findViewById (R.id.btnManchaBlanca);
         nitrogeno=findViewById (R.id.btnNitrogeno);
         roya= findViewById (R.id.btnRoya);
+        panojaloca=findViewById(R.id.btnpanojaloca);
 
         cuarto.setOnClickListener (new View.OnClickListener () {
             @Override
