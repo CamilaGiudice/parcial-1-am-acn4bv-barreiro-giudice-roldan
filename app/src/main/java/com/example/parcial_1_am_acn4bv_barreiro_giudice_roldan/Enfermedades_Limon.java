5a093fc387127eb3df182de7d35e985ff8034425
Camila <cpgiudice@hotmail.com>
2023-11-30 18:33:29 -0300

    Cambios Clase Enfermedades limon: Correccion Url de la nueva enfermedad psorosis

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
@@ -45,7 +45,7 @@ public class Enfermedades_Limon extends AppCompatActivity {
         psorosis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(""));
+               Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://herbariofitopatologia.agro.uba.ar/?page_id=7241"));
                startActivity(intent);
             }
         });
