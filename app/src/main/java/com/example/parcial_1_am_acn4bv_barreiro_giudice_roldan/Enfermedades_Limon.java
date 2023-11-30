7a55f63c0cfa4f0da2d79762bb82068634cbc5d6
monabarreiro <barreiro.monica@gmail.com>
2023-11-29 21:50:34 -0300

    Enfermedades Limon con la inicialización de los botones de enfermedades

--- app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_barreiro_giudice_roldan/Enfermedades_Limon.java
@@ -3,15 +3,17 @@ package com.example.parcial_1_am_acn4bv_barreiro_giudice_roldan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-// HERNAN  ------
+// HERNAN  ------------
 public class Enfermedades_Limon extends AppCompatActivity {
     public Button btnAmain;
     public Button btnAtrigo;
+    private Button antracnosis, acaros,cancrosis,mancha,clorosis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,56 @@ public class Enfermedades_Limon extends AppCompatActivity {
 
         btnAmain=findViewById (R.id.btnAmain);
         btnAtrigo=findViewById (R.id.btnAtrigo);
+
+// Inicializacion de botones de enfermedades Limon
+        antracnosis=findViewById (R.id.btenfantracnosis);
+        acaros= findViewById (R.id.btLimonAcaros);
+        cancrosis=findViewById (R.id.btnLicancrosis);
+        mancha= findViewById (R.id.btnLimancha);
+        clorosis=findViewById (R.id.btnLiclorosis);
+
+        // onclick de todas las enfermedades
+
+        antracnosis.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse
+                        ("https://herbariofitopatologia.agro.uba.ar/?page_id=280"));
+                startActivity (intent);
+            }
+        });
+        acaros.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent (Intent.ACTION_VIEW,Uri.parse
+                        ("https://herbariofitopatologia.agro.uba.ar/?page_id=12058"));
+                startActivity (intent);
+            }
+        });
+        cancrosis.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent (Intent.ACTION_VIEW,Uri.parse
+                        ("https://herbariofitopatologia.agro.uba.ar/?page_id=284"));
+                startActivity (intent);
+            }
+        });
+         mancha.setOnClickListener (new View.OnClickListener () {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent (Intent.ACTION_VIEW,Uri.parse
+                         (" https://herbariofitopatologia.agro.uba.ar/?page_id=302 "));
+                 startActivity (intent);
+             }
+         });
+         clorosis.setOnClickListener (new View.OnClickListener () {
+             @Override
+             public void onClick(View v) {
+                 Intent intent =new Intent (Intent.ACTION_VIEW,Uri.parse
+                         ("https://herbariofitopatologia.agro.uba.ar/?page_id=2825"));
+                 startActivity (intent);
+             }
+         });
 
         btnAmain.setOnClickListener (new View.OnClickListener () {
             @Override
